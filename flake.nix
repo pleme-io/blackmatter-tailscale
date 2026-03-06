beb1eb3bc29f7bfc2a4c3e4abd528f9b099cee1c
drzzln <drzzln@protonmail.com>
2026-03-06 16:00:18 -0500

    feat: add devenv input for enhanced developer environments

--- flake.nix
+++ flake.nix
@@ -1,10 +1,35 @@
 {
   description = "Blackmatter Tailscale - cross-platform Tailscale VPN module";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    devenv = {
+      url = "github:cachix/devenv";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, devenv }:
+  let
+    forAllSystems = nixpkgs.lib.genAttrs [
+      "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"
+    ];
+  in {
     nixosModules.default = import ./module/nixos;
     darwinModules.default = import ./module/darwin;
+
+    devShells = forAllSystems (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      default = devenv.lib.mkShell {
+        inputs = { inherit nixpkgs devenv; };
+        inherit pkgs;
+        modules = [{
+          languages.nix.enable = true;
+          packages = with pkgs; [ nixpkgs-fmt nil ];
+          git-hooks.hooks.nixpkgs-fmt.enable = true;
+        }];
+      };
+    });
   };
 }
